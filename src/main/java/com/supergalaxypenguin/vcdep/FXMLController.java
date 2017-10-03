757bf4d31b6f275f88e7036609ffa5bf2e1e2440
banma <banma@192.168.195.152>
2017-10-03 16:29:24 -0500

    Edited land page, added buttons and text fields

--- src/main/java/com/supergalaxypenguin/vcdep/FXMLController.java
+++ src/main/java/com/supergalaxypenguin/vcdep/FXMLController.java
@@ -5,23 +5,64 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class FXMLController implements Initializable {
     
+    private String gitTxt;
+    private String branchTxt;
+    private String jenkinsTxt;
+    
+    @FXML
+    private ChoiceBox<String> lang;
     @FXML
     private Label label;
+    @FXML
+    private Label testLabel;
+    @FXML
+    private TextField gitUrl;
+    @FXML
+    private TextField branch;
+    @FXML
+    private TextField jenkins;
+    @FXML
+    private Button submit;
+    @FXML
+    private Button browse;
+    @FXML
+    private ImageView penguin;
+    @FXML
+    private ImageView background;
+    
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        testLabel.setText(gitUrl.getText()+"\n"+branch.getText()+"\n"+jenkins.getText());
         // this area will change
+        gitTxt = gitUrl.getText();
+        branchTxt = branch.getText();
+        jenkinsTxt = jenkins.getText();
+        
+        System.out.println(gitTxt+"\n"+branchTxt+"\n"+jenkinsTxt);
+        
+    }
+    @FXML
+    private void handleBrowseButton(ActionEvent event) {
+        System.out.println("Opening File Directory");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-     //this is initialization   
+     lang.getItems().add("PHP");
+     lang.getItems().add("Java");
+     lang.setValue("Java");
+    //this is initialization//   
 
 // TODO
     }    
